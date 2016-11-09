927968be75cdbeefc4ecc2330b907ec3a9b43504
Peter Hoàng <peterhoang@Peters-MacBook-Pro.local>
2016-11-08 21:41:17 -0600

    first commit

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,14 @@
+// word bank
+var countries = ["country1", "country 2"];
+// words guessed
+var guessed = [];
+// number of guesses
+var guesses;
+// number of wins
+var wins;
+// randomly chosen word
+var word = [];
+
+//computer chooses random word
+word = countries[Math.floor(Math.random() * words.length)];
+	return word;
